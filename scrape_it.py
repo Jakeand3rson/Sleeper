adb0906466c9182ea63ca32a5c069418c660c178
Jakealope <jake.anderson486@gmail.com>
2016-03-16 21:05:00 -0700

    added imports and function outlines

--- scrape_it.py
+++ scrape_it.py
@@ -1,1 +1,16 @@
-#http://i.imgur.com/Yal1xei.png
+from bs4 import BeautifulSoup
+import urllib2
+
+
+def pull_data():
+    # This will pull data from the website
+    pass
+
+
+def parse_data():
+    # potential parsing of the data..
+    pass
+
+
+if __name__ == '__main__':
+    pass
