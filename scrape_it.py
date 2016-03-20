7c1aaab841faaf9d9f982d60c2ce9491fd6a2b15
tlake <tanner.lake@gmail.com>
2016-03-20 00:47:06 -0700

    Implemented Selenium authN workaround.

--- scrape_it.py
+++ scrape_it.py
@@ -1,8 +1,7 @@
-from bs4 import BeautifulSoup
-# import urllib2
-import urllib.request as req
-from loginform import fill_login_form
 import os
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 login_email = os.getenv("SLACK_EMAIL")
@@ -15,39 +14,35 @@ urls = {
 }
 
 
-def get_login_page():
-    login_page = req.urlopen(urls['login'])
-    login_soup = BeautifulSoup(login_page, 'html.parser')
-    login_text = BeautifulSoup.prettify(login_soup)
-    return login_text
+def get_page_source_via_selenium():
+    """Use Firefox via Selenium to get page source."""
+    # Initialize browser
+    browser = webdriver.Firefox()
 
+    # Go to the login page
+    browser.get(urls['login'])
 
-def do_login():
-    login_text = get_login_page()
-    login_response = fill_login_form(
-        urls['login'],
-        login_text,
-        login_email,
-        login_password,
-    )
-    return login_response
+    # Get login fields and stuff in login credentials
+    email_field = browser.find_element_by_id("email")
+    password_field = browser.find_element_by_id("password")
+    email_field.send_keys(login_email)
+    password_field.send_keys(login_password, Keys.RETURN)
 
+    # Go to details page now that we're authenticated
+    browser.get(urls['401'])
 
-def pull_data():
-    # This will pull data from the website
-    # # openurl = urllib2.urlopen(
-    #     "https://codefellows.slack.com/messages/sea-python-401d2/details/"
-    # )
-    openurl = req.urlopen(
-        "https://codefellows.slack.com/messages/sea-python-401d2/details/"
-    )
-    soup = BeautifulSoup(openurl, 'html.parser')
-    soup
+    # Get that source, baby!
+    html_source = browser.page_source
+
+    browser.close()
+    return html_source
 
 
 def parse_data():
-    # potential parsing of the data..
-    pass
+    source_string = get_page_source_via_selenium()
+    soup = BeautifulSoup(source_string, 'html.parser')
+
+    # Now do the work of actually parsing the data...
 
 
 if __name__ == '__main__':
