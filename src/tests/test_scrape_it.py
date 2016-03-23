8f4fe4873c8a2dab2054137d57c4f179532e33c2
tlake <tanner.lake@gmail.com>
2016-03-23 16:22:14 -0700

    Parametrized test for if no credentials present.

--- src/tests/test_scrape_it.py
+++ src/tests/test_scrape_it.py
@@ -3,7 +3,17 @@ import os
 from .. import scrape_it
 
 
-def test_missing_email():
-    os.environ['SLACK_EMAIL'] = None
+ENV_VARS = ["SLACK_EMAIL", "SLACK_PASSWORD"]
+
+
+@pytest.mark.parametrize('env_var', ENV_VARS)
+def test_missing_slack_credentials(env_var):
+    """Ensure that we don't start the browser without credentials."""
+    try:
+        del os.environ[env_var]
+    except KeyError:
+        # If the env_var is already missing, we're in the right situation.
+        pass
+
     with pytest.raises(LookupError):
         scrape_it.main()
