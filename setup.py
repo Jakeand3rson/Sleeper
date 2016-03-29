eb817aa122aaef51494b4342330597635addcc7f
tlake <tanner.lake@gmail.com>
2016-03-29 12:43:08 -0700

    'Square-One' refactor.

---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- utf-8 -*-
+
+from setuptools import setup
+
+
+tests_require = [
+    'pytest',
+    'pytest-cov',
+    'tox',
+]
+
+
+setup(
+    name="Sleeper",
+    description="Super secret things.",
+    version=0.1,
+    author="Tanner Lake, Jake Anderson",
+    license="MIT",
+    py_modules=[
+        "scrape_it",
+    ],
+    package_dir={"": "src"},
+    test_suite="test",
+    install_requires=[
+        'slacker',
+    ],
+    extras_require={
+        "test": tests_require,
+    },
+)
