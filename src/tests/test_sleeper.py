63467b68cafba102b9bc374f2f7ac29f074d02a6
Jakealope <jake.anderson486@gmail.com>
2016-03-30 10:02:17 -0700

    init commit, added imports and first fixture

--- src/tests/test_sleeper.py
+++ src/tests/test_sleeper.py
@@ -1,1 +1,9 @@
 # -*- coding: utf-8 -*-
+import pytest
+import sleeper
+
+
+@pytest.fixture(scope='session')
+def correct_login():
+    '''create correct login info'''
+    return os.getenv('SLACK_TOKEN')
