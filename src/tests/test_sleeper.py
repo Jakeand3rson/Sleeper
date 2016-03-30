3b288162acaf30ccb459951ade924cede4861e11
Jakealope <jake.anderson486@gmail.com>
2016-03-30 16:30:21 -0700

    added test to assert the body content of channel_info is correct, passing

--- src/tests/test_sleeper.py
+++ src/tests/test_sleeper.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import pytest
 import os
-from .. sleeper import get_token, channel_id
+from .. sleeper import get_token, channel_id, channel_info
+from slacker import Slacker
 
 channel_name = 'sea-python-401d2'
 
@@ -16,6 +17,13 @@ def test_get_token():
 def test_channel_id():
     """Make sure that the function that gets our channel id is working"""
     slack_token = os.getenv('SLACK_TOKEN')
-    from slacker import Slacker
     s = Slacker(slack_token)
     assert channel_id() == s.channels.get_channel_id(channel_name)
+
+
+def test_channel_info():
+    slack_token = os.getenv('SLACK_TOKEN')
+    s = Slacker(slack_token)
+    channel_id = s.channels.get_channel_id(channel_name)
+    info = s.channels.info(channel_id)
+    assert channel_info().body == info.body
