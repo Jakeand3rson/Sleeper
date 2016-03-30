1997bf43e66a0152c1e5471203ef0e64ffd90dcf
Jakealope <jake.anderson486@gmail.com>
2016-03-30 10:23:05 -0700

    removed fixture and added first test, passing

--- src/tests/test_sleeper.py
+++ src/tests/test_sleeper.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import pytest
-import sleeper
+import os
 
 
-@pytest.fixture(scope='session')
-def correct_login():
-    '''create correct login info'''
-    return os.getenv('SLACK_TOKEN')
+def test_get_token():
+    """Make sure we are indeed getting a token"""
+    """This is assuming you have a slack token in your environmental variables"""
+    slack_token = os.getenv('SLACK_TOKEN')
+    assert slack_token is not None
