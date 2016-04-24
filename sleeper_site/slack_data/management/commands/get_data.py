e9fedc1a04cc4fc47ec7111741f4effe0dd6e8b3
Tanner Lake <tanner.lake@gmail.com>
2016-04-23 21:23:38 -0700

    get_data.py now is a manage.py command that writes to DB.

---
+++ sleeper_site/slack_data/management/commands/get_data.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from django.core.management.base import BaseCommand, CommandError
+from slack_data.models import UserPresence
+
+import os
+from time import sleep
+from datetime import datetime as dt
+from slacker import Slacker
+
+
+class Command(BaseCommand):
+
+    def get_token(self):
+        return os.environ.get('SLACK_TOKEN')
+
+    def channel_id(self):
+        return self.slacker.channels.get_channel_id(self.channel_name)
+
+    def channel_info(self):
+        return self.slacker.channels.info(self.channel_id())
+
+    def get_the_members(self):
+        members = self.channel_info().body['channel']['members']
+        status_dict = {}
+        for m in members:
+            status_dict[m] = self.slacker.users.get_presence(m).body['presence']
+            sleep(1)  # Sleep call to avoid rate-limiting
+        return status_dict
+
+    def save_data(self, data):
+        timestamp = dt.utcnow()
+        for key, val in data.items():
+            presence = UserPresence()
+            presence.timestamp = timestamp
+            presence.slack_id = key
+            presence.active = True if val == 'active' else False
+            presence.save()
+
+    def handle(self, *args, **kwargs):
+        self.channel_name = 'sea-python-401d2'
+        self.slacker = Slacker(self.get_token())
+
+        self.save_data(self.get_the_members())
